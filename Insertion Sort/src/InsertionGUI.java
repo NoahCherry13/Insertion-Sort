aa2508b99c58a38956784360cee6cb48543af4b3
20ncherry <20ncherry@WA-248-9580.schools.westford-ma.gov>
2020-02-04 12:04:05 -0500

    2/4

--- Insertion Sort/src/InsertionGUI.java
+++ Insertion Sort/src/InsertionGUI.java
@@ -1,9 +1,43 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class InsertionGUI {
+import javax.swing.*;
+import BreezySwing.*;
+public class InsertionGUI extends GBFrame implements KeyListener{
 
+	IntegerField inputFld = addIntegerField(0,1,1,1,1);
+	
+	public InsertionGUI() {
+		inputFld.addKeyListener(this);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame frm = new InsertionGUI();
+		frm.setTitle("Ace Program");
+		frm.setSize(700, 500);
+		frm.setVisible(true);
+	}
 
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			System.out.println("Works");
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---
+++ Insertion Sort/src/Sort.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Sort {
+	
+	ArrayList numList = new ArrayList<Integer>();
+	
+	public Sort() {
+		
+	}
+	
+	public void addNumber(int i) {
+		numList.add(i);
+		
+	}
+}
